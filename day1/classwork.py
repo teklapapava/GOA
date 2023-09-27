50eeaaad23db23c3507298d4ed6f163ebce5188b
nika1keshelava <nika.keshelava.1@btu.edu.ge>
2023-09-27 23:37:56 +0400

    first commit

---
+++ day1/classwork.py
@@ -0,0 +1,7 @@
+age1=50
+age2=25
+age3=21
+
+print(age1)
+print(age2)
+print(age3)
---
+++ day1/homework.py
@@ -0,0 +1,28 @@
+tekla=210
+salome=220
+
+print(tekla+50)
+print(salome+50)
+
+city1="qutaisi"
+city2="tbilisi"
+city3="batumi"
+
+print(city1)
+
+
+item=5
+amount=3
+total=item * amount
+
+print(total)
+
+item=36
+amount=9
+total=item / amount
+print(total)
+
+item=40
+amount=9
+total= item //amount
+print(total)
