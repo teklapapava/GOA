f2ccef349e66d614f6de2f872055aa93145e9d57
teklapapava <145158384+teklapapava@users.noreply.github.com>
2023-09-30 23:36:18 +0400

    Add files via upload

--- homeworks/homework1.py
+++ homeworks/homework1.py
@@ -1,3 +1,19 @@
 name = input()
 surname = input()
 age = input()
+
+
+
+
+
+
+name = input("your_name")
+surname = input("your_surname")
+age = input("your_age")
+print("your_name" + name + "your_surname" + surname + "age" + age)
+
+
+
+marta = input("how old are you")
+new_age = age + 25 
+print(new_age)
