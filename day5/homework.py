9c00553d5bf3d884146663966d8154a660ad0b9a
teklapapava <145158384+teklapapava@users.noreply.github.com>
2023-10-10 19:00:04 +0400

    Add files via upload

--- day5/homework.py
+++ day5/homework.py
@@ -2,11 +2,11 @@
  #დაუპრინტეთ ყოველ წელს რამდენჯერ უფროსი იქნება მამამისი მასზე.
 
 
-age = input("Enter your age : ")
-age1 = input("Enter you Dad's age : " )
-age2 = input("Enter how mant times my dad's will be older than me : ")
+age = int(input("Enter your age : "))
+age1 = int(input("Enter you Dad's age : " ))
+age2 = int(input("Enter how many times my dad's will be older than me : "))
 for i in range(10):
-    print(age + str(age + i) + age1 + str(age1 + i) + "he will be" + age2 + "older than me")
+    print("I am " + str(age + i) + "my dad's" + str(age1 + i) + "he will be" + str(age2) + "older than me" )
 
 
 
@@ -14,11 +14,11 @@ for i in range(10):
     #0იდან 30ის ფარგლებში დაპრინტეთ ყველა ლუწი და კენტი რიცხვი, მიუწერეთ გვერდზე კენტია თუ ლუწი
 
 
-#seats = 30
-#while seats>0 :
- #   print(str(seats)+"even")
-  #  seats = seats -1
-   # print(str(seats)+"odd")
-    #seats = seats -1
+seats = 30
+while seats>0 :
+    print(str(seats)+"even")
+    seats = seats -1
+    print(str(seats)+"odd")
+    seats = seats -1
  
  
