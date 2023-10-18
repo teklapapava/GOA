c4cfbd01a55cf22555e02cb8089bd38bf6bdf618
teklapapava <145158384+teklapapava@users.noreply.github.com>
2023-10-18 21:24:21 +0400

    Add files via upload

---
+++ day8/homework.py
@@ -0,0 +1,54 @@
+#1)
+#შექმენით სია, ამ სიაში ჩაწერეთ სხვადასხვა ციფრები (1, 2, 3, 4, ასე არა, რამე მაგ: 45, 23, 87, 55,)
+# და გამოიტანეთ ამ სიაში ყველა რიცხვის ჯამი, ასევე ამავე სიიდან უნდა დაპრინტოთ ყველა რიცხვი ცალ ცალკე, 
+#და მიუწეროთ ლუწია თუ კენტი.
+
+#num=[45, 23, 88, 55, 72,]
+#print(int(num[0])+int(num[1])+int(num[2])+int(num[3])+int(num[4]))
+#print(str(num[0]) + "odd" + str(num[1]) + "odd" + str(num[2]) + "even" + str(num[3]) + "odd" + str(num[4]) + "even")
+
+#2)
+#ბილეთი ღირს 25 ლარი,
+#მაგრამ ბავშვებისთვის უფასოა,
+#გვყავს 13 ადამიანი, აქედან 10 დიდი და 3 ბავშვი, 
+#გამოთვალეთ ჯამში რამდენი ლარი დასჭირდებათ 
+
+#Ticket = int(input("ticket price"))
+#Quantity = int(input("How many people do we have"))
+#print(Ticket * Quantity)
+
+#3
+#მომხმარებელს შემოატანინეთ სახელი,
+#და დაპრინტეთ ეს სახელი იმდენჯერ, რამდენი ასოც არის სახელში.
+
+#seats = 5
+#while seats>0:
+ #   print(str(seats)+"Tekla")
+  #  seats = seats -1
+
+#4)
+#GOA ში ყოველ შემოყვანილ ადამიანზე გეძლევა 100ლ.
+#მომხმარებელს შემოატანინეთ თუ რამდენი ადამიანი შემოიყვანა ხოლო თითო შემოყვანილ ადამიანზე დაერიცხოს 100ლ
+#1) რამდენი დაგერიცხება თუ შემოიყვან 10 ბავშვს?  15 ბავშცს?
+#2) რამდენი დაგერიცხება თუ შემოიყვან 100 ბავშვს გამოკლებული 37 დამატებული 13 გაყოფილი 10 და 
+#გამრავებული 264 ზე
+
+#salary = int("100")
+#user=int(input("Enter how many person did you bring"))
+#print(salary * user)
+#user1=int(input("Enter how many person did you bring"))
+#print(salary * user1)
+
+#salary = int("100")
+#user=int(input("Enter how many child did you bring"))
+#print(salary * user)
+#salary1 = int("10000")
+#print(salary1 - 37 + 13 / 10 * 264)
+
+#5)
+#HARDEST BOSS:
+#დაპრინტეთ hello world (ოღონდ არა ასე print("hello world")  )
+
+list=["h", "e", "l", "l", "o", "w", "o", "r", "l", "d"]
+print(str(list[0])+str(list[1])+str(list[2])+str(list[3])+str(list[4])  +  str(list[5])+str(list[6])+
+      str(list[7])+str(list[8])+str(list[9]))
